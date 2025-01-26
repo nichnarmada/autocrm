d552a8ae3e0b7346d8b606e085cdd261ee1b7c9f
Nicholas Narmada <nichnarmada@gmail.com>
2025-01-26 16:17:09 -0500

    fixed build error

--- components/users/invite-user-dialog.tsx
+++ components/users/invite-user-dialog.tsx
@@ -7,11 +7,9 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { UserPlus } from "lucide-react"
 
 interface InviteUserDialogProps {
   open?: boolean
